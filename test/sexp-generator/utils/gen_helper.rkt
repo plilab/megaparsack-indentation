e7a2e3437e0f3dea43700905effbc2e4f73f1311
CYX22222003 <e1155533@u.nus.edu>
2025-10-03 15:17:22 +0800

    Implement initial version of gen_helper
    * Issue: it has infinite loop

--- test/sexp-generator/utils/gen_helper.rkt
+++ test/sexp-generator/utils/gen_helper.rkt
@@ -3,7 +3,10 @@
 (require "./utils.rkt")
 (provide gen-item)
 
-;;; Generating items
+;;; -------------------------
+;;; Helper generators
+;;; -------------------------
+
 (define (gen-num [min-num 0] [max-num 999])
   (choose-integer min-num max-num))
 
@@ -22,7 +25,31 @@
                       (delay
                         (gen-string-fixed 5)))))
 
+(define (gen-generator-list generator [recurse-limit 2])
+  (bind-generators ([rand (choose-integer 0 1)] [recurse? (and (positive? recurse-limit) (= 0 rand))]
+                                                [head (generator)]
+                                                [tail
+                                                 (if recurse?
+                                                     (gen-generator-list generator
+                                                                         (sub1 recurse-limit))
+                                                     (list))])
+                   (append (list head) tail)))
+
+;;; -------------------------
+;;; Forward declarations
+;;; -------------------------
+(define gen-item null)
+(define gen-group null)
+(define gen-block null)
+(define gen-alts null)
+
+(define gen-item-list null)
+(define gen-group-list null)
+(define gen-block-list null)
+
+;;; --------------------------
 ;;; Generate items
+;;; --------------------------
 ;;; atom
 (define (gen-atom)
   (choose-mixed (list (delay
@@ -34,57 +61,123 @@
   (bind-generators ([a (choose-one-of (list '+ '- '* '/ '= '==))]) (list 'op a)))
 
 ;;; (parens group)
-(define (gen-parens [limit 5]) (error "unimplemented"))
+(define (gen-parens)
+  (bind-generators ([grps (gen-group-list)]) (append (list 'parens) grps)))
 
 ;;; (brackets group)
-(define (gen-brackets [limit 5]) (error "unimplemented"))
+(define (gen-brackets)
+  (bind-generators ([grps (gen-group-list)]) (append (list 'brackets) grps)))
 
 ;;; (braces group)
-(define (gen-braces [limit 5]) (error "unimplemented"))
+(define (gen-braces)
+  (bind-generators ([grps (gen-group-list)]) (append (list 'braces) grps)))
 
 ;;; (quotes group)
-(define (gen-quotes [limit 5]) (error "unimplemented"))
+(define (gen-quotes [limit 5])
+  (bind-generators ([grps (gen-group-list)]) (append (list 'quotes) grps)))
 
-(define (gen-item)
-  (choose-mixed (list (delay
-                        (gen-atom))
-                      (delay
-                        (gen-operator)))))
+(set! gen-item
+      (lambda ()
+        (choose-mixed (list (delay
+                              (gen-atom))
+                            (delay
+                              (gen-operator))
+                            (delay
+                              (gen-parens))
+                            (delay
+                              (gen-brackets))
+                            (delay
+                              (gen-braces))
+                            (delay
+                              (gen-quotes))))))
 
-;;; Small debug
-(print_random_generated_vals gen-item)
+(set! gen-item-list (lambda () (gen-generator-list gen-item)))
 
-;;; Generate gropus
+;;; ----------------------
+;;; Generate groups
+;;; ----------------------
+
 ;;; (group item* item)
-(define (gen-group1 [recurse-limit 5])
+(define (gen-group1 [recurse-limit 2])
   (bind-generators ([rand (choose-integer 0 1)] [recurse? (and (positive? recurse-limit) (= 0 rand))]
                                                 [a (gen-item)]
                                                 [res
                                                  (if recurse?
-                                                     (gen-group1 (sub1 recurse-limit))
+                                                     (gen-item-list)
                                                      (list))])
                    (append (list 'group a) res)))
 
 ;;; (group item* block)
-(define (gen-group2 [recurse-limit 5])
-  (error "not implemented"))
+(define (gen-group2)
+  (bind-generators ([rand (choose-integer 0 1)] [recurse? (= 0 rand)]
+                                                [b (gen-block)]
+                                                [i (gen-item)]
+                                                [res
+                                                 (if recurse?
+                                                     (gen-item-list)
+                                                     (list i))])
+                   (append (list 'group) res (list b))))
 
 ;;; (group item* alts)
-(define (gen-group3 [recurse-limit 5])
-  (error "not implemented"))
+(define (gen-group3)
+  (bind-generators ([rand (choose-integer 0 1)] [recurse? (= 0 rand)]
+                                                [a (gen-alts)]
+                                                [i (gen-item)]
+                                                [res
+                                                 (if recurse?
+                                                     (gen-item-list)
+                                                     (list i))])
+                   (append (list 'group) res (list a))))
 
 ;;; (group item* block alts)
-(define (gen-group4 [recurse-limit 5] (error "not implemented"))
-  )
+(define (gen-group4)
+  (bind-generators ([rand (choose-integer 0 1)] [recurse? (= 0 rand)]
+                                                [a (gen-alts)]
+                                                [b (gen-block)]
+                                                [i (gen-item)]
+                                                [res
+                                                 (if recurse?
+                                                     (gen-item-list)
+                                                     (list i))])
+                   (append (list 'group) res (list b a))))
 
-(define (gen-group)
-  (choose-mixed (list (delay
-                        (gen-group1))
-                      (delay
-                        (gen-group2))
-                      (delay
-                        (gen-group3))
-                      (delay
-                        (gen-group4)))))
+(set! gen-group
+      (lambda ()
+        (choose-mixed (list (delay
+                              (gen-group1))
+                            (delay
+                              (gen-group2))
+                            (delay
+                              (gen-group3))
+                            (delay
+                              (gen-group4))))))
 
-(print_random_generated_vals gen-group1)
+(set! gen-group-list (lambda () (gen-generator-list gen-group)))
+
+;;; --------------------
+;;; Gen block
+;;; --------------------
+(set! gen-block (lambda ([recurse-limit 2])
+      (bind-generators ([rand (choose-integer 0 1)] [recurse?
+                                                     (and (positive? recurse-limit) (= 0 rand))]
+                                                    [res
+                                                     (if recurse?
+                                                         (gen-group-list)
+                                                         (list))])
+                       (append (list 'block) res))))
+
+(set! gen-block-list (lambda () (gen-generator-list gen-block)))
+
+;;; --------------------
+;;; Gen alts
+;;; --------------------
+(set! gen-alts (lambda ([recurse-limit 2])
+      (bind-generators ([rand (choose-integer 0 1)] [recurse?
+                                                     (and (positive? recurse-limit) (= 0 rand))]
+                                                    [res
+                                                     (if recurse?
+                                                         (gen-block-list)
+                                                         (list))])
+                       (append (list 'alts) res))))
+
+(print_random_generated_vals gen-group)
--- test/sexp-generator/utils/utils.rkt
+++ test/sexp-generator/utils/utils.rkt
@@ -3,5 +3,5 @@
 
 (provide print_random_generated_vals)
 
-(define (print_random_generated_vals gen-generator [n 10])
+(define (print_random_generated_vals gen-generator [n 500])
   (with-test-count n (quickcheck (property ([ele (gen-generator)]) (displayln ele) #t))))
