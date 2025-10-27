107e94e6c5899b7db19c8e9d337e24f43a026cd9
CYX22222003 <e1155533@u.nus.edu>
2025-10-27 13:34:39 +0800

    Gracefully handle the case where generated sexp is malformed

--- test/sexp-generator/utils/utils.rkt
+++ test/sexp-generator/utils/utils.rkt
@@ -8,17 +8,32 @@
 
 (provide pretty_print_random_generated_vals)
 
-(define (pretty_print_sexp s [test-port (open-output-string)])
-  (write-shrubbery s (current-output-port) #:pretty? #t #:armor? #f #:prefer-multiline? #t)
-  ;;; Verify the s exp; will fails if it does not work
-  (write-shrubbery s test-port #:pretty? #t #:armor? #f #:prefer-multiline? #t)
+(define (pretty_print_sexp s)
+  ;; Temporary port for verification
+  (define test-port (open-output-string))
+  
+  ;; Write to test-port first, to check parsing
+  (write-shrubbery s test-port
+                   #:pretty? #t
+                   #:armor? #f
+                   #:prefer-multiline? #t)
+  
+  ;; Attempt to parse
   (define out-str (get-output-string test-port))
   (define in-port (open-input-string out-str))
-  (parse-all in-port)
-  (newline)
-  (newline))
+  
+  (with-handlers ([exn:fail? 
+                   (lambda (e)
+                     (displayln (format "Parse failed: ~a" (exn-message e)))
+                     #f)])
+    (when (parse-all in-port)
+      ;; Only write to current-output-port if parse succeeds
+      (write-shrubbery s (current-output-port)
+                       #:pretty? #t
+                       #:armor? #f
+                       #:prefer-multiline? #t)
+      (newline)
+      (newline))))
 
-(define (pretty_print_random_generated_vals gen-generator [test-port (open-output-string)] [n 100])
-  (with-test-count
-   n
-   (quickcheck (property ([ele (gen-generator)]) (pretty_print_sexp ele test-port) #t))))
+(define (pretty_print_random_generated_vals gen-generator [n 2500])
+  (with-test-count n (quickcheck (property ([ele (gen-generator)]) (pretty_print_sexp ele) #t))))
