642d29d2b076e436c4e32ee982dea670cd361ecb
miffi <miffi@decadent.rest>
2026-03-06 13:31:15 +0800

    handle quote without separator

--- megaparsack-indentation-shrubbery/main.rkt
+++ megaparsack-indentation-shrubbery/main.rkt
@@ -1,7 +1,6 @@
 #lang racket/base
 
 (require (except-in racket/base do))
-(require racket/function)
 (require racket/syntax-srcloc)
 (require data/monad)
 (require data/applicative)
@@ -36,8 +35,15 @@
 (define (token/p name)
   (label/p
    (symbol->string name)
-   (do [token <- (indent/p (satisfy/p (lambda (x) (and (token? x) (eq? (token-name x) name)))))]
+   (do
+     [token <- (indent/p (satisfy/p (lambda (x) (and (token? x) (eq? (token-name x) name)))))]
      (pure (syntax->datum (token-value token))))))
+
+(define (lexeme/p name)
+  (do
+    [token <- (token/p name)]
+    (or/p whitespace/p void/p)
+    (pure token)))
 
 (define (token-pred/p name pred)
   (define (satisfy-pred token)
@@ -46,25 +52,33 @@
          (pred (syntax->datum (token-value token)))))
   (label/p
    (symbol->string name)
-   (do [token <- (indent/p (satisfy/p satisfy-pred))]
+   (do
+     [token <- (indent/p (satisfy/p satisfy-pred))]
      (pure (syntax->datum (token-value token))))))
 
-(define (token-string=/p name str)
-  (token-pred/p name (lambda (x) (string=? x str))))
+(define (lexeme-pred/p name pred)
+  (do
+    [token <- (token-pred/p name pred)]
+    (or/p whitespace/p void/p)
+    (pure token)))
 
-(define (insensitive-token/p name)
-  (label/p
-   (symbol->string name)
-   (do [token <- (satisfy/p (lambda (x) (and (token? x) (eq? (token-name x) name))))]
-     (pure (syntax->datum (token-value token))))))
-
+(define (lexeme-string=/p name str)
+  (lexeme-pred/p name (lambda (x) (string=? x str))))
 
 (define atom/p
   (label/p
    "atom"
-   (or/p (token/p 'identifier)
-         (token/p 'literal)
-         (token/p 'operator))))
+   (or/p (lexeme/p 'identifier)
+         (lexeme/p 'literal)
+         (lexeme/p 'operator))))
+
+;; (define (at/p #:inline inline)
+;;   (define command)
+;;   (do
+;;     (lexeme/p 'at)
+;;     (cond
+;;       [inline])))
+  
 
 (define (string-last str)
   (string-ref str (sub1 (string-length str))))
@@ -72,35 +86,41 @@
 (define newline/p
   (label/p
    "newline"
-   (insensitive-token/p 'whitespace)))
+   (local-indentation/p '* (lexeme-pred/p 'whitespace (lambda (value) (eq? #\newline (string-last value)))))))
+
+(define whitespace/p
+  (label/p
+   "newline"
+   (local-indentation/p '* (lexeme-pred/p 'whitespace (lambda (value) (not (eq? #\newline (string-last value))))))))
 
 (define line-continuation/p
-  (do (token/p 'continue-operator) newline/p))
+  (do
+    (lexeme/p 'continue-operator) newline/p))
 
 (define newlines/p
   (noncommittal/p (many/p newline/p)))
 
 (define group-line
   (do
-      [atoms <- (many+/p (or/p atom/p (delay/p opener-closers/p)))]
+    [atoms <- (many+/p (or/p atom/p (delay/p opener-closers/p)))]
     (pure atoms)))
 
 ;; Ask if we want to have continuations parsed with * local indentation
 (define group-line-with-continuation
   (do
-      [first-line <- group-line]
+    [first-line <- group-line]
     [continuations <- (local-indentation/p '* (many/p (do line-continuation/p group-line)))]
     (pure (apply append first-line continuations))))
 
 (define group-fragment
   (do
-      [first-line-with-continuation <- group-line-with-continuation]
+    [first-line-with-continuation <- group-line-with-continuation]
     [operator-continuations <- (local-indentation/p
                                 '>
                                 (many/p
                                  (try/p (do
-                                            newlines/p
-                                          [op <- (absolute-indentation/p (token/p 'operator))]
+                                          newlines/p
+                                          [op <- (absolute-indentation/p (lexeme/p 'operator))]
                                           [operator-line-with-continuations <- group-line-with-continuation]
                                           (pure (cons op operator-line-with-continuations))))))]
     (pure (apply append first-line-with-continuation operator-continuations))))
@@ -108,7 +128,7 @@
 (define (group/p-generator #:is-in-alt is-in-alt)
   (define colon-with-block/p
     (do
-        (token/p 'block-operator)
+      (lexeme/p 'block-operator)
       (cond
         [is-in-alt (or/p block-in-alt/p (do newlines/p block/p))]
         [else (do newlines/p block/p)])))
@@ -116,21 +136,23 @@
   (or/p
    ; Group exists
    (do
-       [group <- group-fragment]
+     [group <- group-fragment]
      (or/p
 
       ; => Group with empty block and alts
-      (try/p (do (token/p 'block-operator)
+      (try/p (do
+               (lexeme/p 'block-operator)
                newlines/p
                [alts <- alts/p]
                (pure `(group ,@group ,alts))))
 
       ; Group and Block exist
-      (do [block <- colon-with-block/p]
+      (do
+        [block <- colon-with-block/p]
         (or/p
          ; => Group, Block, and Alt
          (try/p (do
-                    newlines/p
+                  newlines/p
                   [alts <- alts/p]
                   (pure `(group ,@group ,block ,alts))))
          ; => Group and Block
@@ -140,7 +162,7 @@
       (or/p
        ; => Group with Alt on another line
        (try/p (do
-                  newlines/p
+                newlines/p
                 [alts <- alts/p]
                 (pure `(group ,@group ,alts))))
        (cond
@@ -149,16 +171,17 @@
          ; => Group with Alt on the same line
          [else (or/p
                 (try/p (do
-                           [alts <- (local-indentation/p '> alts/p)]
+                         [alts <- (local-indentation/p '> alts/p)]
                          (pure `(group ,@group ,alts))))
                 (pure `(group ,@group)))]))))
 
    ; Group does not exist
-   (do [block <- colon-with-block/p]
+   (do
+     [block <- colon-with-block/p]
      (or/p
       ; => Block and Alts
       (try/p (do
-                 newlines/p
+               newlines/p
                [alts <- alts/p]
                (pure `(group ,block ,alts))))
       ; => Block
@@ -169,47 +192,43 @@
 
 
 (define (make-opener-closer/p identifier opener closer separator/p #:newline-separated [newline-separated #f])
+  (define line-separator
+    (cond
+      [newline-separated (or/p (do newlines/p separator/p newlines/p) (do newline/p newlines/p))]
+      [else (do newlines/p separator/p newlines/p)]))
+
   (do
-      (token-string=/p 'opener opener)
+    (lexeme-string=/p 'opener opener)
+    newlines/p
     [groups <- (local-indentation/p
                 '>
-                (sep-end-by/p (do newlines/p
+                (sep-end-by/p (do
                                 [groups <- (absolute-indentation/p
                                             (many/p group/p #:sep (noncommittal/p separator/p)))]
-                                newlines/p
                                 (pure groups))
-                              (cond
-                                [newline-separated (or/p separator/p void/p)]
-                                [else separator/p])))]
-    (local-indentation/p '* (token-string=/p 'closer closer))
+                              line-separator))]
+    newlines/p
+    (local-indentation/p '* (lexeme-string=/p 'closer closer))
     (pure (cons identifier (apply append groups)))))
 
 (define comma/p
-  (label/p "," (token/p 'comma-operator)))
+  (label/p "," (lexeme/p 'comma-operator)))
 (define semicolon/p
-  (label/p ";" (token/p 'semicolon-operator)))
+  (label/p ";" (lexeme/p 'semicolon-operator)))
 
 (define parens/p (make-opener-closer/p 'parens "(" ")" comma/p))
 (define brackets/p (make-opener-closer/p 'brackets "[" "]" comma/p))
 (define braces/p (make-opener-closer/p 'braces "{" "}" comma/p))
-(define quotes/p (make-opener-closer/p 'quotes "'" "'" semicolon/p))
-
-;; I don't know enough macros to generate opener-closer/p as succinct as this
-; (define associations
-;   '(
-;     ('parens "(" ")" 'comma-operator)
-;     ('brackets "[" "]" 'comma-operator)
-;     ('braces "{" "}" 'comma-operator)
-;     ('quotes "'" "'" 'semicolon-operator)
-;     ('quotes "'«" "»'" 'semicolon-operator)
-;     ))
+(define quotes/p (make-opener-closer/p 'quotes "'" "'" semicolon/p #:newline-separated #t))
+(define quotes-alternate/p (make-opener-closer/p 'quotes "'«" "»'" semicolon/p #:newline-separated #t))
 
 (define opener-closers/p
   (or/p
    parens/p
    brackets/p
    braces/p
-   quotes/p))
+   quotes/p
+   quotes-alternate/p))
 
 ;; The entire document. This always produces one multi constructor.
 ;; This does not parse a #lang line at the beggining of a file. TODO Create
@@ -218,7 +237,7 @@
 ;; https://docs.racket-lang.org/shrubbery/group-and-block.html are the same thing?
 (define document/p
   (do
-      newlines/p
+    newlines/p
     [groups <- (sep-end-by/p (absolute-indentation/p group/p) newlines/p)]
     eof/p
     (pure (cons 'multi groups))))
@@ -226,40 +245,39 @@
 (define block/p
   (local-indentation/p '>
                        (do
-                           [groups <- (many+/p (try/p (absolute-indentation/p group/p)) #:sep newlines/p)]
+                         [groups <- (many+/p (try/p (absolute-indentation/p group/p)) #:sep newlines/p)]
                          (pure `(block . ,groups)))))
 
 (define block-in-alt/p
   (local-indentation/p '>
                        (do
-                           [leading-group <- (absolute-indentation/p group-in-alt/p)]
+                         [leading-group <- (absolute-indentation/p group-in-alt/p)]
                          newlines/p
                          [groups <- (many/p (try/p (absolute-indentation/p group/p)) #:sep newlines/p)]
                          (pure `(block ,leading-group . ,groups)))))
 
 (define alts/p
   (do
-      [alts <- (many+/p
-                (absolute-indentation/p
-                 (many+/p (try/p (do
-                                     (token/p 'bar-operator)
-                                   [block <- block-in-alt/p]
-                                   newlines/p
-                                   (pure block))))))]
+    [alts <- (many+/p
+              (absolute-indentation/p
+               (many+/p (try/p (do
+                                 (lexeme/p 'bar-operator)
+                                 [block <- block-in-alt/p]
+                                 newlines/p
+                                 (pure block))))))]
     (pure (cons 'alts (apply append alts)))))
 
 
 (define (lex str)
-  (define (whitespace-or-comment-token? token)
+  (define (comment-token? token)
     (and (token? token)
-         (or (and (eq? (token-name token) 'whitespace) (not (eq? #\newline (string-last (token-e token)))))
-             (eq? (token-name token) 'comment))))
+         (eq? (token-name token) 'comment)))
 
   (define input-port (open-input-string str))
   (port-count-lines! input-port)
 
   (filter
-   (lambda (x) (not (whitespace-or-comment-token? x)))
+   (lambda (x) (not (comment-token? x)))
    (lex-all input-port (lambda (token explanation) (raise (cons token explanation))))))
 
 (define (run-parser-on-lexed parser str)
@@ -269,7 +287,7 @@
   (run-parser-on-lexed document/p str))
 
 (module+ main
-  (displayln (lex "@typeset{Write @bold{\"hello\"}}")))
+  (displayln (lex "#{ 1234 }")))
 
 
 (module+ test
@@ -293,10 +311,10 @@
   (check-equal? (shrubbery-parser "a\n|    b: d\n|c") (success '(multi (group a (alts (block (group b (block (group d)))) (block (group c)))))) "Alt then group")
   (check-equal? (shrubbery-parser "a\n| d\n  | c") (success '(multi (group a (alts (block (group d (alts (block (group c))))))))) "Alt in alt")
   (check-equal? (shrubbery-parser "a\n| d | c") (success '(multi (group a (alts (block (group d)) (block (group c)))))) "Inline alt")
-  (check-equal? (shrubbery-parser "a\n| b | c\n| d") (success '(multi (group a (alts (block (group b)) (block (group c)) (block (group d)))))) "Alt in multiple same line alt")
+  (check-equal? (shrubbery-parser "a\n| b | c\n| d") (success '(multi (group a (alts (block (group b)) (block (group c)) (block (group d)))))) "Alt in multiple same line alt"))
   ; (check-equal? (shrubbery-parser "a\n| b\nc\n| d") (success '(multi (group a (alts (block (group b)))) (group c (alts (block (group d)))))) "Multiple alts")
   ; (check-equal? (shrubbery-parser "   a\nb") (failure '(multi (group a) (group b))) "Two groups")
   ; (check-equal? (shrubbery-parser "a\n   b") (failure '(multi (group a) (group b))) "Two groups")
   ; (check-equal? (shrubbery-parser "a b c d\n\n   e f g h") (failure '(multi (group a b c d) (group e f g h))) "Groups should start on same indentation")
   ; (check-equal? (shrubbery-parser "a b: d\nc") (success '(multi (group a b (block (group d))) (group c))) "block then group")
-  )
+  
